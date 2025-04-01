fece81f4a01080731c82024986cedb2460da6760
reyreyhtml <reyreyhtml@gmail.com>
2025-04-01 23:04:44 +0000

    inicio do JavaScript

---
+++ main.js
@@ -0,0 +1,25 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+let buttonSelected = null
+
+
+let billInput = document.querySelector("#bill")
+billInput.addEventListener("input", receiveBillValue)
+
+
+let numberOfPeopleInput = document.querySelector("#people")
+numberOfPeopleInput.addEventListener("input", receiveNumberOfPeopleValue)
+
+
+function receiveBillValue() {
+    bill = billInput.valueAsNumber
+
+    calculate()
+}
+
+function receiveNumberOfPeopleValue() {
+    numberOfPeople = numberOfPeopleInput.valueAsNumber
+
+    calculate()
+}
